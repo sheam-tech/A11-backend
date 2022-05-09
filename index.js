be9e31e21a2b0f8483ce189c4d273f38c24e4a30
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-05-09 18:27:28 +0600

    MongoDB Database linked

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
+// -------------mongoDb----------
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gdwjv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 

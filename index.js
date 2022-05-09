1c1b2b346dc5794e3a9a51ab926f356d34936ef1
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-05-09 18:30:29 +0600

    JWT feature added

--- index.js
+++ index.js
@@ -3,18 +3,50 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-
+const jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
 app.use(express.json());
-// -------------mongoDb----------
+
+app.post("/login", (req, res) => {
+  const user = req.body;
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+    expiresIn: "5d",
+  });
+  res.send({ accessToken });
+});
+
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gdwjv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// -------------------------------------
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+// -------------------------------------
+// verify Json Web Token :
+function verifyJwt(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send([{ message: "Authorization filed" }]);
+  }
 
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send([{ message: "Please provide a valid token." }]);
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
